ea683dab4d9cd9e4a08b6d7eb67be162dce288de
Marek Bernat <marek.bernat@ixperta.com>
2016-08-29 20:40:36 +0200

    Initial commit

---
+++ sdl.ml
@@ -0,0 +1,19 @@
+#use "topfind";;
+#require "tsdl.top";;
+#require "result";;
+
+open Tsdl
+open Result
+
+let main () = match Sdl.init Sdl.Init.video with
+| Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
+| Ok () ->
+    match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
+    | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
+    | Ok w ->
+        Sdl.delay 3000l;
+        Sdl.destroy_window w;
+        Sdl.quit ();
+        exit 0
+
+let () = main ()
